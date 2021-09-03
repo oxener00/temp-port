988a6de1ca2cf415feda51215214db081e5b292e
oxener00 <oxener00@gmail.com>
2021-09-03 17:16:29 +0700

    Deploy

---
+++ src/data.js
@@ -0,0 +1,99 @@
+export const projects = [
+  {
+    title: "Birthday Reminder",
+    subtitle: "React",
+    description: "Simple list of birthday.",
+    image: "./project-1.gif",
+    link: "https://affectionate-brown-53e40b.netlify.app",
+  },
+  {
+    title: "Tours",
+    subtitle: "React",
+    description: "List of tours that can be removed and refreshed.",
+    image: "./project-2.gif",
+    link: "https://gracious-turing-4f7eed.netlify.app",
+  },
+  {
+    title: "Reviews",
+    subtitle: "React",
+    description: "Cards of reviews that can be changed and random.",
+    image: "./project-3.gif",
+    link: "https://tender-goldberg-cd4f23.netlify.app",
+  },
+  {
+    title: "Accordians",
+    subtitle: "React",
+    description: "Simple accordian menu.",
+    image: "./project-4.gif",
+    link: "https://stupefied-bell-793aa6.netlify.app/",
+  },
+  {
+    title: "Menu",
+    subtitle: "React",
+    description: "Simple pages of menu with filter.",
+    image: "./project-5.png",
+    link: "https://pedantic-beaver-0e9b9a.netlify.app/",
+  },
+  {
+    title: "Slider",
+    subtitle: "React",
+    description: "Slider of reviews",
+    image: "./project-6.png",
+    link: "https://competent-lumiere-40f3c6.netlify.app/",
+  },
+  {
+    title: "Lorem Ipsum",
+    subtitle: "React",
+    description: "Lorem ipsum with paragraph input",
+    image: "./project-7.png",
+    link: "https://cranky-wilson-738ede.netlify.app/",
+  },
+  {
+    title: "Color Generator",
+    subtitle: "React",
+    description: "Simple color generator with hex code input",
+    image: "./project-8.png",
+    link: "https://xenodochial-sammet-e85d7a.netlify.app/",
+  },
+  {
+    title: "Grocery List",
+    subtitle: "React",
+    description:
+      "Grocery list that can add items and remove items from the list.",
+    image: "./project-9.png",
+    link: "https://amazing-rosalind-2d9cde.netlify.app/",
+  },
+  {
+    title: "Navbar",
+    subtitle: "React",
+    description: "Simple navbar that is linked to somewhere else.",
+    image: "./project-10.png",
+    link: "https://practical-fermi-81692f.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
